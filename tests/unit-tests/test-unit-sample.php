253ba225be83dd37a411c794f841eeaeb35c40b5
Marieka van Vuuren <mariekavv@gmail.com>
2016-09-08 11:54:54 +0200

    second commit

--- tests/unit-tests/test-unit-sample.php
+++ tests/unit-tests/test-unit-sample.php
@@ -15,8 +15,18 @@ class UnitTests extends \PHPUnit_Framework_TestCase{
 			use PHPMock;
 		
 		public function test_wcct_get_option() {
-			$this->assertEquals('river-club', wcct_get_option('venue'));
-			}
+						
+			$get_option = $this->getFunctionMock('WCCT16',
+			'get_option');
+			
+			$get_option->expects($this->once())
+			->with($this->equalTo('wcct_venue'))
+			->willReturn('river-club');
+			
+			$this->assertEquals('river-club', wcct_get_option(
+			'venue'));
+		}
+		
 		}
 		
 		$this->assertTrue( true );
