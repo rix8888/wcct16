616a19a57ac21633583d8bbe327b6ee90abe86a4
Marieka van Vuuren <mariekavv@gmail.com>
2016-09-08 11:45:14 +0200

    #1

--- tests/unit-tests/test-unit-sample.php
+++ tests/unit-tests/test-unit-sample.php
@@ -11,6 +11,14 @@ class UnitTests extends \PHPUnit_Framework_TestCase{
 	 */
 	function test_sample() {
 		// Replace this with some actual testing code.
+		class UnitTests extends \PHPUnit_Framework_TestCase {
+			use PHPMock;
+		
+		public function test_wcct_get_option() {
+			$this->assertEquals('river-club', wcct_get_option('venue'));
+			}
+		}
+		
 		$this->assertTrue( true );
 	}
 }
